37d0989b8057847735f48b80a453aa0db05166ac
Nik Maydanyuk <nikmaydan@Niks-MacBook-Pro.local>
2023-06-24 14:23:15 -0400

    adding Day7 code and files

---
+++ Day7/Day7.py
@@ -0,0 +1,21 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+    
+    result = []
+    for ele in arr:
+        result = [ele] + result
+        
+    for ele in result:
+        print(ele, end = ' ')
