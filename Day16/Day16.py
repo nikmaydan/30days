467e4b4ddb1f31b2595cb2a0b61966a5ff72699b
Nik Maydanyuk <nikmaydan@Niks-MacBook-Pro.local>
2023-07-05 18:42:16 -0400

    adding Day16 stuff

---
+++ Day16/Day16.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+def toInteger(S):
+    try:
+        S = int(S)
+        print(S)
+    except:
+        print("Bad String")
+                
+        
+S = input()
+toInteger(S)
