2cb600fb1fc71deeda6ed8eb7017d85ebd51da47
Nik Maydanyuk <nikmaydan@Niks-MacBook-Pro.local>
2023-07-07 17:54:34 -0400

    adding Day19 solution)

---
+++ Day19/Day19.py
@@ -0,0 +1,22 @@
+class AdvancedArithmetic(object):
+    def divisorSum(n):
+        raise NotImplementedError
+
+class Calculator(AdvancedArithmetic):
+    def divisorSum(self, n):
+        rangeN = n
+        a = []
+        for i in range(1, rangeN+1):
+            if n%i == 0:
+                a.append(i)
+        return(sum(a))
+            
+    
+    
+
+
+n = int(input())
+my_calculator = Calculator()
+s = my_calculator.divisorSum(n)
+print("I implemented: " + type(my_calculator).__bases__[0].__name__)
+print(s)
